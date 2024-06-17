1aadf506be298f3990f93d5020a11c924eef5f7a
Zayd Ftouh <zaydftouh79@gmail.com>
2024-06-17 22:47:45 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,22 +10,6 @@ export default function Footer() {
               Copyright © {new Date().getFullYear()}. All rights are reserved
             </h3>
             <div className="footerc__socials">
-              <a
-                aria-label="linkedin"
-                target="_blank"
-                rel="noreferrer"
-                href="https://www.linkedin.com/in/zayd-ftouh/"
-              >
-                <IconBrandLinkedin width={30} height={30} />
-              </a>
-              <a
-                aria-label="github"
-                target="_blank"
-                rel="noreferrer"
-                href="https://github.com/ftouuh"
-              >
-                <IconBrandGithub width={30} height={30} />
-              </a>
             </div>
           </div>
         </div>
