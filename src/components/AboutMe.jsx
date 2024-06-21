b93131ece6d455b1171f56e06430dee0f969ac46
Zayd Ftouh <zaydftouh79@gmail.com>
2024-06-21 15:00:58 +0100

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,4 +1,3 @@
-
 import { useEffect } from 'react';
 import '../styles/comps/about.css'
 import Aos from 'aos';
@@ -9,28 +8,27 @@ export default function AboutMe() {
   })
   return (
     <>
-{/*       <section className="about" id="about">
+      <section className="about" id="about">
         <div className="container">
           <div className="about-content" data-aos='fade-up'>
-          <div className="img-side">
+            <div className="img-side">
               <img src='https://en.idei.club/uploads/posts/2023-08/thumbs/1690933294_en-idei-club-p-coding-room-setup-dizain-instagram-14.jpg' alt="mee" className="img-side__main-img" />
             </div>
             <div className="text-side">
-              <h3>About me</h3>
+              <h3>About Us</h3>
               <h4>
-                Professional Video Editor <br /> based in Tetouan, Morocco 📍
+                Welcome to Scalezare. 📍 <br />
+                Helping Content Creators and Businesses Thrive
               </h4>
               <p>
-                Hey, my name is Zayd, and I make cool videos for cool people.
+                Hi, we are Scalezare, and we specialize in helping content creators and businesses market their brands organically through social media. 
                 <br />
                 <br />
               </p>
             </div>
           </div>
         </div>
-      </section> */}
+      </section>
     </>
   );
 }
-
-
